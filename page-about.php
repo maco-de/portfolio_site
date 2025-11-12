75851f8dbf1e8f597c67ef5da8748a06fe40d89a
maco-de <mako_mako_makoto@hotmail.com>
2025-11-12 23:44:20 +0900

    HTML・JSの調整3
    JS背景追加の為

--- page-about.php
+++ page-about.php
@@ -2,44 +2,44 @@
 
 <main>
     <div class="about">
+        <div class="about__title">
+            <h1>ABOUT</h1>
+            <span>maco-deについて</span>
+        </div>
+        <div class="waves waves_bg-w">
+            <svg
+                class="waves_item"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28"
+                preserveAspectRatio="none"
+                shape-rendering="auto">
+                <defs>
+                    <path
+                        id="gentle-wave"
+                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                </defs>
+                <g class="parallax">
+                    <use
+                        xlink:href="#gentle-wave"
+                        x="48"
+                        y="0"
+                        fill="rgba(240,240,240,0.7)" />
+                    <use
+                        xlink:href="#gentle-wave"
+                        x="48"
+                        y="3"
+                        fill="rgba(240,240,240,0.5)" />
+                    <use
+                        xlink:href="#gentle-wave"
+                        x="48"
+                        y="5"
+                        fill="rgba(240,240,240,0.3)" />
+                    <use xlink:href="#gentle-wave" x="48" y="7" fill="rgba(240,240,240)" />
+                </g>
+            </svg>
+        </div>
         <div id="bubble-container" class="bubble-container">
-            <div class="about__title">
-                <h1>ABOUT</h1>
-                <span>maco-deについて</span>
-            </div>
-            <div class="waves waves_bg-w">
-                <svg
-                    class="waves_item"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 24 150 28"
-                    preserveAspectRatio="none"
-                    shape-rendering="auto">
-                    <defs>
-                        <path
-                            id="gentle-wave"
-                            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                    </defs>
-                    <g class="parallax">
-                        <use
-                            xlink:href="#gentle-wave"
-                            x="48"
-                            y="0"
-                            fill="rgba(240,240,240,0.7)" />
-                        <use
-                            xlink:href="#gentle-wave"
-                            x="48"
-                            y="3"
-                            fill="rgba(240,240,240,0.5)" />
-                        <use
-                            xlink:href="#gentle-wave"
-                            x="48"
-                            y="5"
-                            fill="rgba(240,240,240,0.3)" />
-                        <use xlink:href="#gentle-wave" x="48" y="7" fill="rgba(240,240,240)" />
-                    </g>
-                </svg>
-            </div>
             <section class="about__rule">
                 <div class="content-width">
                     <h2 class="about__rule__title">コーディングルール</h2>
